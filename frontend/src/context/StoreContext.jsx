27ebabb9e59308784edab9989ba28815f05da856
NagarajuTL <shashinagaraju17@gmail.com>
2025-05-31 09:10:15 +0530

    url update

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -6,7 +6,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "https://food-delivery-backend-5b6g.onrender.com";
+  const url = "http://3.84.18.208:5000";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
